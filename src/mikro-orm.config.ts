c611397e307385135624e0332848bc4a2f442914
unknown <oluwaseunadams17@gmail.com>
2022-08-07 19:31:15 +0100

    register

--- src/mikro-orm.config.ts
+++ src/mikro-orm.config.ts
@@ -1,16 +1,17 @@
-import { __prod__ } from "./constants";
-import { Post } from "./entities/Post";
-import { MikroORM } from "@mikro-orm/core";
+import { __prod__ } from './constants';
+import { Post } from './entities/Post';
+import { MikroORM } from '@mikro-orm/core';
 import path from 'path';
+import { User } from './entities/User';
 
 export default {
-  migrations :{
+  migrations: {
     path: path.join(__dirname, './migrations'),
     pattern: /^[\w-]+\d+\.[tj]s$/,
   },
-  entities: [Post],
+  entities: [Post, User],
   dbName: 'fsreddit',
   type: 'postgresql',
   debug: !__prod__,
-  password:"emmanuel2001",
+  password: 'emmanuel2001',
 } as Parameters<typeof MikroORM.init>[0];
